0921561a0dbde582a7af03e520fbf8f78e555b56
Md Mainul Hasan <mainul1945@gmail.com>
2026-03-28 09:08:23 +0600

    call

--- src/lib/components/Footer.js
+++ src/lib/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="min-h-[4vh] bg-base-300 flex justify-center space-x-10 items-center">
       <h4 className=" p-3">All rights preserved by MainulTech </h4>
-      <Link href={'tel:+8801743914780'}><MdAddCall /> : Call</Link>
+      <Link className=" flex" href={'tel:+8801743914780'}><MdAddCall /> : Call</Link>
       <div>
       </div>
     </div>
