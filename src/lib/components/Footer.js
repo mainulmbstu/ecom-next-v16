18d106478848218576953150aea83c59af1636d1
Md Mainul Hasan <mainul1945@gmail.com>
2026-03-28 08:58:07 +0600

    call

--- src/lib/components/Footer.js
+++ src/lib/components/Footer.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import { MdAddCall } from "react-icons/md";
 
 const Footer = () => {
   return (
     <div className="min-h-[4vh] bg-base-300 flex justify-between items-center">
       <h4 className=" p-3">All rights preserved by MainulTech </h4>
       <div>
-        <Link href={'tel:+8801743914780'}></Link>
+        <Link href={'tel:+8801743914780'}><MdAddCall /> : Call</Link>
       </div>
     </div>
   );
