beea5a1529e10c31c1e7d9cd1492f6275205b680
Md Mainul Hasan <mainul1945@gmail.com>
2026-04-20 16:09:22 +0600

    Update swalModal.js

--- src/lib/helpers/swalModal.js
+++ src/lib/helpers/swalModal.js
@@ -3,19 +3,20 @@ import Swal from "sweetalert2";
 export const swalModal = (
   message = "Successsful",
   icon = "success",
-  timer = icon === "error" ? false : 3000,
+  // timer = icon === "error" ? false : 3000,
+  timer = 3000,
 ) => {
   let eTimer = icon === "error" ? false : timer;
   return Swal.fire({
     icon: icon, // warning, info, question, error
     text: message,
     position: "top-start",
-    timer: timer,
+    timer: eTimer,
     customClass: {
       popup: "md:ms-auto swalmodal  ",
       // popup: "swalmodal",
     },
-    showConfirmButton: timer ? false : true,
+    showConfirmButton: eTimer ? false : true,
     // title: "Your work has been saved",
     width: "90vw",
   });
